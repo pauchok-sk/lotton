a41a0e954f1a16b148febf6d3cf3a3fe3204232f
pauchok-sk <pauchoooooook@gmail.com>
2024-12-15 17:44:39 +0300

    header

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as functions from "./files/functions.js";
+
+import "../scss/style.scss";
+import "./bootstrap.min.js"
+import spoller from "./files/spoller.js";
+
+functions.isWebp();
+spoller();
