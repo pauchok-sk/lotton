179d7797035f2570fbb0183715a155e8085eda0e
pauchok-sk <pauchoooooook@gmail.com>
2024-12-23 23:50:09 +0300

    repair end

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,8 @@ import copy from "./files/copy.js";
 import burger from "./files/burger.js";
 import date from "./files/date.js";
 import ranges from "./files/ranges.js";
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css'; // optional for styling
 
 spoller();
 tab();
@@ -13,3 +15,5 @@ copy();
 burger();
 date();
 ranges();
+
+tippy('[data-tippy-content]');
